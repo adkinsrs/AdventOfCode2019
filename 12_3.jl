b3b02bded4c0efeaef73002c0c7f049178cefec6
Shaun Adkins <sadkins@som.umaryland.edu>
2019-12-03 09:27:53 -0500

    starting day 3 puzzle

---
+++ 12_3.jl
@@ -0,0 +1,19 @@
+#!/usr/bin/env julia
+
+"""
+I don't feel like copy/pasting the whole set of instructions here, since it makes the code feel bloated.
+Instead of I will just link to the puzzle's URL, like so:
+
+https://adventofcode.com/2019/day/3
+"""
+
+function main1()
+
+end
+
+function main2()
+	nothing
+end
+
+main1()
+main2()
