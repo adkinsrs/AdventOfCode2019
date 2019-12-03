43e5c7c57720d3f5b4f3ec8d29d475959e830236
Shaun Adkins <shaun.adkins@gmail.com>
2019-12-03 01:31:30 +0000

    solved day 2

--- 12_2.jl
+++ 12_2.jl
@@ -49,21 +49,123 @@ Once you have a working computer, the first step is to restore the gravity assis
 
 What value is left at position 0 after the program halts?
 """
+
+"""
+--- Part Two ---
+"Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+
+"However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output 19690720. 
+What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+"""
+
+function init_positions!(positions, noun=12, verb=2)
+    # Julia is 1-indexed so these are slightly different than the instructions
+    positions[2] = noun   # Noun
+    positions[3] = verb    # Verg
+end
+
+function resolve_opcode_1!(positions, window)
+    # Windows 2, 3, and 4 are memory address positions
+    total = positions[window[2]+1] + positions[window[3]+1]
+    positions[window[4]+1] = total
+end
+
+function resolve_opcode_2!(positions, window)
+    total = positions[window[2]+1] * positions[window[3]+1]
+    positions[window[4]+1] = total
+end
+
+function resolve_opcode_99(positions)
+    return positions[1]
+end
+
 function main1()
-    # Getting curl 400 error so downloading file beforehand
-    #input_file = download("https://adventofcode.com/2019/day/1/input")
-    input_file = joinpath(pwd(), "files", "12_1_input.txt")
-    total_fuel = zero(Int)
-    open(input_file, "r") do ifh
-        lines = [strip(line) for line in eachline(ifh)]
-        total_fuel = sum(calculate_fuel_required, lines)
+    input_file = joinpath(pwd(), "files", "12_2_input.txt")
+    intcode = open(input_file, "r") do ifh
+        read(ifh, String)
     end
+    
+    #intcode = "1,9,10,3,2,3,11,0,99,30,40,50"  # Test string
+    intcode = strip(intcode)
+    str_positions = split(intcode, ",")
+    positions = map(x -> parse(Int, x), str_positions)
+
+    init_positions!(positions)
+
+    final_position = length(positions) - 3
+    for i in 1:4:final_position
+        window = positions[i:i+3]
+        if window[1] == 1
+            resolve_opcode_1!(positions, window)
+        elseif window[1] == 2
+            resolve_opcode_2!(positions, window)
+        elseif window[1] == 99
+            println("Part 1 Answer:")
+            @show resolve_opcode_99(positions)
+            return
+        end
+    end
+
+    # In case we never have opcode 99
     println("Part 1 Answer:")
-    @show Int(total_fuel)
+    @show positions[1]
 end
 
 function main2()
-    nothing
+    input_file = joinpath(pwd(), "files", "12_2_input.txt")
+    intcode = open(input_file, "r") do ifh
+        read(ifh, String)
+    end
+    
+    intcode = strip(intcode)
+    str_positions = split(intcode, ",")
+    positions = map(x -> parse(Int, x), str_positions)
+    orig_positions = copy(positions)
+
+    DESIRED_OUTPUT = 19690720
+    final_position = length(positions) - 3
+
+    # Compute output over range of nouns and verbs until the combination
+    # that gives the desired output is come across
+    for noun in 0:99, verb in 0:99
+        init_positions!(positions, noun, verb)
+        output = zero(Int)
+        for i in 1:4:final_position
+            window = positions[i:i+3]
+            if window[1] == 1
+                resolve_opcode_1!(positions, window)
+            elseif window[1] == 2
+                resolve_opcode_2!(positions, window)
+            elseif window[1] == 99
+                output = resolve_opcode_99(positions)
+                break
+            end
+        end
+        if output == DESIRED_OUTPUT
+            println("Part 2 Answer:")
+            @show (100 * noun + verb)
+            exit()
+        end
+        positions = copy(orig_positions)
+    end
+
+    # In case we don't find it...
+    println("Didn't find the combination :-(")
+
 end
 
 main1()
