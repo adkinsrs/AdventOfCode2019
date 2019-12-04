71f45f7fb9c8544c63b71ec44e0ad675164a0151
Shaun Adkins <sadkins@som.umaryland.edu>
2019-12-04 09:50:43 -0500

    adding template file so I don't have to copy from a finished file

---
+++ template.jl
@@ -0,0 +1,16 @@
+#!/usr/bin/env julia
+
+function main1()
+    # Getting curl 400 error so downloading file beforehand
+    #input_file = download("https://adventofcode.com/2019/day/1/input")
+    input_file = joinpath(pwd(), "files", "12_1_input.txt")
+    lines = eachlines(open(input_file, "r"))
+    println("Part 1 Answer:")
+end
+
+function main2()
+    nothing
+end
+
+main1()
+main2()
