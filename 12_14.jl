f9aee7eedf013c467866232e2b90785f7e3553ea
Shaun Adkins <shaun.adkins@gmail.com>
2019-12-14 16:59:10 +0000

    I just can't figure out the math here...

--- 12_14.jl
+++ 12_14.jl
@@ -4,25 +4,54 @@
 https://adventofcode.com/2019/day/14
 """
 
+function calculate_requirements!(requirements, reactions, product)
+    """Recursively calculate the number required to make FUEL for each reactant."""
+    product == "ORE" && return
+    # Create intermediate requirements dictionary for current product
+    reactants = map(x -> x.reactant, reactions[product]["inputs"])
+
+    [requirements[input.reactant] += input.quantity for input in reactions[product]["inputs"]]
+    @show product, reactions[product]["quantity"], reactions[product]["inputs"]
+    [calculate_requirements!(requirements, reactions, input.reactant) for input in reactions[product]["inputs"]]
+    @show requirements
+    @show ""
+
+    #product_requirements = Dict( input => zero(Int) for input in reactants)
+    #[product_requirements[input.reactant] = input.quantity for input in reactions[product]["inputs"]]
+
+    #for reactant in collect(keys(product_requirements))
+    #    if reactant != "ORE"
+    #        inner_requirements = calculate_requirements!(product_requirements, reactions, reactant)
+    #        @show product, inner_requirements
+    #    end
+    #end
+    # How much product will be produced in the reaction
+    #product_requirements[product] = reactions[product]["quantity"]
+
+    #return product_requirements
+end
+
 function parse_reactants(reactant_string)
     """Parse a string of reactants."""
     reactants = split(reactant_string, ", ")
     inputs = []
     for r in reactants
         quantity, reactant = split(r, " ")
-        push!(inputs, (reactant=reactant, quantity=quantity))
+        push!(inputs, (reactant=reactant, quantity=parse(Int,quantity)))
     end
     return inputs
 end
 
 function main1()
     input_file = joinpath(pwd(), "input.txt")
+    input_file = joinpath(pwd(), "testinput.txt")
+
     lines = readlines(open(input_file, "r"))
     lines = map(l -> chomp(l), lines)
     # Determined via a test that all outputs are unique (num lines = num unique chemicals)
     reactants = map(x -> split(x, " => ")[1], lines)
     products = map(x -> split(x, " => ")[2], lines)
-    quantities = map(x -> split(x, " ")[1], products)
+    quantities = map(x -> parse(Int, split(x, " ")[1]), products)
     chems = map(x -> split(x, " ")[2], products)
 
     # Break reactant_string into quantities and inputs
@@ -31,15 +60,14 @@ function main1()
     # Store everything into a dictionary
     reactions = Dict( chems[i] => Dict("quantity" => quantities[i], "inputs" => inputs[i]) for i in 1:length(chems))
 
-    num_ore = 0
+    # Keep track of chemical requirements to make FUEL
+    requirements = Dict( chem => zero(Int) for chem in chems)
+    requirements["ORE"] = 0 # ORE is not in list of products
     curr_chem = "FUEL"
-
-    for input in reactions[curr_chem]["inputs"]
-        @show reactions[input.reactant]["inputs"] 
-    end
-
-
+    calculate_requirements!(requirements, reactions, curr_chem)
+    #@show requirements
     println("Part 1 Answer:")
+    @show requirements["ORE"]
 end
 
 function main2()
